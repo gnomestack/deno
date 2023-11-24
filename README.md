d780151765bb81faa9a1f0c90419c58d3ba270e1
bowtiedgnome <119234633+bowtiedgnome@users.noreply.github.com>
2023-11-24 15:18:16 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gnomestack for Deno
+
+Work less. Do more.
+
+![logo](./.eng/assets/icon.png)
+
+Gnome stacks for Deno extends the deno standard ("std") library.
---
+++ std/deps.ts
@@ -0,0 +1,10 @@
+export * from "https://deno.land/std@0.200.0/fmt/colors.ts";
+export { BufWriterSync } from "https://deno.land/std@0.200.0/io/buf_writer.ts";
+export type { Writer } from "https://deno.land/std@0.200.0/types.d.ts";
+export * from "https://deno.land/x/retry@v2.0.0/mod.ts";
+export * from "https://deno.land/std@0.200.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.200.0/fmt/printf.ts";
+export { deepMerge } from "https://deno.land/std@0.200.0/collections/deep_merge.ts";
+export { parse as parseFlags } from "https://deno.land/std@0.200.0/flags/mod.ts";
+export * as dotenv from "https://deno.land/std@0.200.0/dotenv/mod.ts";
+export { parse as parseYaml, stringify as stringifYaml } from "https://deno.land/std@0.200.0/yaml/mod.ts";
