f4d6878108f91d1be61bff87272dd971d6430192
bowtiedgnome <119234633+bowtiedgnome@users.noreply.github.com>
2023-11-24 20:46:41 -0600

    import std code

--- std/mod_info.ts
+++ std/mod_info.ts
@@ -1,3 +1,3 @@
-export const VERSION = "0.0.8";
-export const AUTHORS = ["bearz-me"];
+export const VERSION = "0.0.1";
+export const AUTHORS = ["BowTiedGnome"];
 export const STD_VERSION = "0.200.0";
